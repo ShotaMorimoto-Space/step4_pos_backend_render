f84a86b2476db688f999765273472b5e26ba4e4e
ShotaMorimoto-Space <s.morimoto1101@gmail.com>
2025-07-08 12:55:04 +0900

    cros

--- main.py
+++ main.py
@@ -9,7 +9,8 @@ app = FastAPI()
 # CORS設定を追加
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Next.js 開発サーバー
+    allow_origins=["http://localhost:3000",
+                   "https://step4-pos-frontend-render.onrender.com"],  # Next.js 開発サーバー
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
